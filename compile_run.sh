27fe6fdc82daa11fe0a8bdda2bfbb0bb968e1e4a
Lorea Louis <louis.lorea@etudiant.univ-rennes1.fr>
2024-04-28 17:58:24 +0200

    update for compile with lib

--- compile_run.sh
+++ compile_run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-g++ src/sources/*.cc src/headers/*.h `sdl2-config --cflags --libs` -o DarksoulIV
+g++ src/sources/*.cc src/headers/*.h `sdl2-config --cflags --libs` -lSDL2_image -o DarksoulIV
 ./DarksoulIV
